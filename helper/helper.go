24e2bcf3f19e7b997a254b6a046dffe1bd472aa5
bagusprakasa <47969692+bagusprakasa@users.noreply.github.com>
2022-08-11 09:59:46 +0700

    update check email

--- helper/helper.go
+++ helper/helper.go
@@ -3,21 +3,21 @@ package helper
 import "github.com/go-playground/validator/v10"
 
 type Response struct {
-	Meta Meta `json:"meta"`
+	Meta Meta        `json:"meta"`
 	Data interface{} `json:"data"`
 }
 
 type Meta struct {
 	Message string `json:"message"`
-	Code int `json:"code"`
-	Status string `json:"status"`
+	Code    int    `json:"code"`
+	Status  string `json:"status"`
 }
 
 func APIResponse(message string, code int, status string, data interface{}) Response {
 	meta := Meta{
 		Message: message,
-		Code: code,
-		Status: status,
+		Code:    code,
+		Status:  status,
 	}
 
 	jsonRespone := Response{
